f28f32dd0b4e196827ef82117dc29510bd43d626
Peter Ingles <peter.ingles@gmail.com>
2023-08-12 23:48:39 -0500

    Image description toggle added

--- src/components/GalleryList/GalleryItem.jsx
+++ src/components/GalleryList/GalleryItem.jsx
@@ -1,6 +1,22 @@
+import { useState } from "react"
+
+
+
 function GalleryItem({imageProp}) {
+
+    const [descriptionStatus, setDescriptionStatus] = useState(false);
+
+    function showDescription(){
+        console.log("CLICKED ON IMAGE", descriptionStatus)
+        setDescriptionStatus(!descriptionStatus)
+    }
+
+    if(descriptionStatus == true){
+        return <p onClick={() => showDescription()}>{imageProp.description}</p>
+    }
+
     return (
-        <img src={`${imageProp.path}`} alt={`${imageProp.description}`}  />
+        <img onClick={() => showDescription()} src={`${imageProp.path}`} alt={`${imageProp.description}`}  />
 )}
 
 export default GalleryItem
