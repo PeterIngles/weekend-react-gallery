3b0f2e847d0dd500c5c5450c3e9793cb8b0754f1
Peter Ingles <peter.ingles@gmail.com>
2023-08-12 20:31:38 -0500

    GET working

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,7 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/bright_eyes.jpg', description: `I'm Wide Awake It's Morning by Bright Eyes`, likes:0},
+    { id: 3, path: 'images/pheobe.jpg', description: `Punisher by Phoebe Bridgers`, likes: 0}
 ];
 
 module.exports = galleryItems;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,42 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios'
+
 import './App.css';
 
 function App() {
+
+  let [imageGallery, setImageGallery] = useState([]);
+
+  useEffect(() => {
+    getImages();
+  });
+
+  const getImages = () => {
+    axios
+      .get('/gallery')
+      .then((response) => {
+        // array of inventory objects saved to state array
+        setImageGallery(response.data);
+      })
+      .catch((error) => {
+        alert(`Couldn't get imageGallery. Try again later`);
+        console.log('Error getting imageGallery', error);
+      });
+  };
+
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <>
+  {imageGallery.map(image => 
+    <img key = {image.id} src={`${image.path}`} alt="gallery"/>
+  )}
+</>
       </div>
     );
 }
