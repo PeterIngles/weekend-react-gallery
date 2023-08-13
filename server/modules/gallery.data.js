8612a7eda65003c48e5a782c286506003c1e4d41
Peter Ingles <peter.ingles@gmail.com>
2023-08-13 15:15:06 -0500

    Added more to gallery, changed title

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,7 +1,13 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 1, path: 'images/its-almost-dry.jpeg', description: `It's Almost Dry by Pusha T`, likes: 0 },
     { id: 2, path: 'images/bright_eyes.jpg', description: `I'm Wide Awake It's Morning by Bright Eyes`, likes:0},
-    { id: 3, path: 'images/pheobe.jpg', description: `Punisher by Phoebe Bridgers`, likes: 0}
+    { id: 3, path: 'images/alfredo.webp', description: `Alfredo by Freddie Gibbs`, likes: 0},
+    { id: 4, path: 'images/pheobe.jpg', description: `Punisher by Phoebe Bridgers`, likes: 0},
+    { id: 5, path: 'images/ants-from-up-there.jpeg', description: `Ants From Up There by Black Country, New Road`, likes: 0},
+    { id: 6, path: 'images/run-the-jewels2.webp', description: `Rune the Jewels 2 by Run the Jewels`, likes: 0},
+    { id: 8, path: 'images/morbid-stuff.jpeg', description: `Morbid Stuff by Pup`, likes: 0},
+    { id: 9, path: 'images/is-this-it.jpeg', description: `Is This It by The Strokes`, likes: 0},
+    { id: 10, path: 'images/whatever-people.jpeg', description: `Whatever People Say I Am That's What I'm Not by Arctic Monkeys`, likes: 0}
 ];
 
 module.exports = galleryItems;
