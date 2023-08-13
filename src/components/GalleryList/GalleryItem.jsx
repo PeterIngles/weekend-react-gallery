98fec1d60f50250c298dc8b5de397124bfa413f1
Peter Ingles <peter.ingles@gmail.com>
2023-08-13 14:19:27 -0500

    PUT refreshing correctly

--- src/components/GalleryList/GalleryItem.jsx
+++ src/components/GalleryList/GalleryItem.jsx
@@ -2,42 +2,31 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function GalleryItem({ imageProp }) {
-  const [likeCount, setLikeCount] = useState(imageProp.likes); // Initialize likeCount with 0
-
-  useEffect(() => {
-    axios
-      .get(`/gallery/likes/${imageProp.id}`) // Replace with the appropriate API endpoint
-      .then((response) => {
-        setLikeCount(response.data.likes); // Set the initial likeCount based on the server response
-      })
-      .catch((error) => {
-        console.log("Error fetching likeCount:", error);
-      });
-  }, []);
-
+  const [likeCount, setLikeCount] = useState(imageProp.likes);
   const [descriptionStatus, setDescriptionStatus] = useState(false);
 
+  useEffect(() => {
+    console.log("Like count changed:", likeCount);
+  }, [likeCount]);
+
+
   function showDescription() {
-    console.log("CLICKED ON IMAGE", descriptionStatus);
     setDescriptionStatus(!descriptionStatus);
   }
 
   const clickLike = () => {
-    setLikeCount(likeCount + 1); // Increment likeCount on button click
-  };
-
-  useEffect(() => {
-    console.log("Like count changed:", likeCount);
-
     axios
-      .put(`/gallery/like/${imageProp.id}`, { likes: likeCount })
+      .put(`/gallery/like/${imageProp.id}`)
       .then((response) => {
-        console.log("Response from PUT:", response);
+        console.log("Response", response)
+        console.log("Current likeCount:", likeCount); // Log current likeCount state
+        setLikeCount(imageProp.likes); // Update likeCount based on the server response
+        console.log("Updated likeCount:", likeCount); // Log updated likeCount state
       })
       .catch((error) => {
         console.log("Error on PUT:", error);
       });
-  }, [likeCount]);
+  };
 
   if (descriptionStatus === true) {
     return (
@@ -45,7 +34,7 @@ function GalleryItem({ imageProp }) {
         <div className="image-item">
           <p onClick={showDescription}>{imageProp.description}</p>
           <button onClick={clickLike}>LIKE 👍</button>
-          <p>LIKES: {likeCount}</p>
+          <p>LIKES: {imageProp.likes}</p>
         </div>
       </>
     );
@@ -60,7 +49,7 @@ function GalleryItem({ imageProp }) {
           alt={`${imageProp.description}`}
         />
         <button onClick={clickLike}>LIKE 👍</button>
-        <p>LIKES: {likeCount}</p>
+        <p>LIKES: {imageProp.likes}</p>
       </div>
     </>
   );
