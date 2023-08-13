e18aba9fb1efd22a324560089a0ea523751c8cf6
Peter Ingles <peter.ingles@gmail.com>
2023-08-13 00:43:58 -0500

    Added Like button

--- src/components/GalleryList/GalleryItem.jsx
+++ src/components/GalleryList/GalleryItem.jsx
@@ -1,9 +1,8 @@
 import { useState } from "react"
 
-
-
 function GalleryItem({ imageProp }) {
 
+    const [likeCount, setLikeCount] = useState(0)
     const [descriptionStatus, setDescriptionStatus] = useState(false);
 
     function showDescription() {
@@ -11,16 +10,27 @@ function GalleryItem({ imageProp }) {
         setDescriptionStatus(!descriptionStatus)
     }
 
-    if (descriptionStatus == true) {
-        return <p onClick={() => showDescription()}>{imageProp.description}</p>
-
+    function clickLike(){
+        setLikeCount(likeCount + 1)
     }
 
+    if (descriptionStatus == true) {
+        return (
+            <>
+                <div className='image-item'>
+                    <p onClick={() => showDescription()}>{imageProp.description}</p>
+                    <button onClick={() => clickLike()}>LIKE 👍</button>
+                    <p>LIKES:{likeCount}</p>
+                </div>
+            </>
+        )
+    }
     return (
         <>
             <div className='image-item'>
-            <img onClick={() => showDescription()} src={`${imageProp.path}`} alt={`${imageProp.description}`} />
-            <button>LIKE 👍</button>
+                <img onClick={() => showDescription()} src={`${imageProp.path}`} alt={`${imageProp.description}`} />
+                <button onClick={() => clickLike()}>LIKE 👍</button>
+                <p>LIKES:{likeCount}</p>
             </div>
         </>
     )
